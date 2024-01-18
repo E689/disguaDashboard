2bbf90273dff9d98a1210121a9a906aefc3d66ee
Alejandro Cortes <ale.crts1996@gmail.com>
2024-01-18 02:48:30 -0800

    Added Dashboard Layout.

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,13 +1,12 @@
-import { ThemeToggle } from "@/components/ThemeToggle"
 
 export default function UserAuthLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
   return (
     <div>
-      <ThemeToggle />
       {children}
     </div>
   )
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,16 @@
+'use client'
+
+import { ThemeToggle } from "@/components/ThemeToggle"
+import { Button } from "@/components/ui/button"
+import { signOut } from "next-auth/react"
+
+export default function Dashboard() {
+
+  return (
+    <main className="w-screen h-[300vh]">
+      <ThemeToggle />
+      Hola mundo desde el Dashboard!
+      <Button onClick={() => signOut()}>Iniciar Sesión</Button>
+    </main>
+  )
+}
